53692de7bd3cc3512a828d479c3f59bc7841d075
Yonatan Kof <[kof@yonatankof.com]>
2020-08-03 22:21:37 +0300

    Remove side padding on mobile

--- src/components/Author.vue
+++ src/components/Author.vue
@@ -36,7 +36,10 @@ export default {
     max-width: calc(var(--spacem-8xl) * 6);
     text-align: center;
     padding: var(--spacem-md) var(--content-space);
- 
+    @media screen and (max-width: 480px) {
+        // Remove padding on small screens
+        padding: var(--spacem-md) 0;
+    } 
     &__logo {
         width: calc(var(--spacem-7xl) * 2);
         height: calc(var(--spacem-7xl) * 2);
