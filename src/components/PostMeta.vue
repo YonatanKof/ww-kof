98455579246d52ad7b8ce4c604b6264554d89bd8
Yonatan Kof <[kof@yonatankof.com]>
2020-08-04 18:46:08 +0300

    Read minutes as inline block

--- src/components/PostMeta.vue
+++ src/components/PostMeta.vue
@@ -2,7 +2,7 @@
     <div class="post-meta">
         Written by <em>{{ post.author_name }}</em> • Posted {{ post.date }} •
         <template v-if="post.timeToRead">
-            <em>{{ post.timeToRead }} min read.</em>
+            <em>{{ post.timeToRead }} min read</em>
         </template>
     </div>
 </template>
@@ -17,5 +17,8 @@ export default {
 .post-meta {
     font-size: 0.8em;
     opacity: 0.8;
+    em {
+        display: inline-block;
+    }
 }
 </style>
